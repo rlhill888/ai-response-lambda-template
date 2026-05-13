25095c9beb6d61b76543ec6b88c10b13cf4a544e
rlhill888 <rlhill888@gmail.com>
2026-05-13 04:34:45 -0400

    Switch to esbuild bundle - single file deployment

--- dist/lib/ably/ablyRest.js
+++
@@ -1,3 +0,0 @@
-import Ably from "ably";
-export const ablyRest = new Ably.Rest(process.env.ABLY_API_KEY);
-export const COMPANY_NAME_PLACEHOLDER_CHAT_CHANNEL = "COMPANY_NAME_PLACEHOLDER-chat-updates";
--- dist/lib/prompt/aiPrompt.js
+++
@@ -1,6 +0,0 @@
-export default function generatePrompt(AiPrompt) {
-    const MessagingServiceExplanation = "You are a professional AI assistant for this business. Respond in a helpful, warm, and professional tone. Keep answers concise, accurate, and focused on helping the customer with their question or next step";
-    const flatMessageExplanation = "Sometimes you will need to flag a message as needs human intervention. If for any of the reasons below you need to flag a message, it is very important that you make this response return EXACTLY this: $!$MESSAGE_NEEDS_HUMAN_INTERVENTION$!$";
-    const finalPrompt = `${MessagingServiceExplanation} \n \n ${AiPrompt.AIResponsePrompt} \n \n ${flatMessageExplanation} \n \n ${AiPrompt.FlagUserInLoopRequirements}`;
-    return finalPrompt;
-}
